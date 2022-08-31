d72a7aca906ada2f89e2698201fe5a3f3ab4807a
Engin Gülek <engingulek0@gmail.com>
2022-08-31 19:21:04 +0300

    APIFilter:CommentAuthFilter

--- model/Panel.js
+++ model/Panel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const panelSchema = new mongoose.Schema({
-    userId: {type:Number,require:true},
+    userId: {type:String,require:true},
     text : {type:String,require:true},
     date : {type:String,require:true},
     comment : {type:Array,require:true},
+    areaId:{type:Number,require:true},
     
 })
 
--- router/panel.js
+++ router/panel.js
@@ -1,14 +1,83 @@
 const express = require("express");
 const Panel  = require("../model/Panel")
+const Auth  = require("../model/Auth")
 const router = express.Router()
 
 
 /// MARK: API
 
+
 router.get("/allPanel",(req,res)=>{
+
+    var comment = []
+    
     Panel.find()
     .then(panels=>{
-        res.json(panels)
+        panels.forEach((panel)=>{
+            Auth.find()
+            .then(auths=>{
+                var panelAuths = auths.filter(auth=> auth._id == panel.userId)
+                panelAuths.forEach((panelAuth)=>{
+                    panel.comment.forEach((panelComment) => {
+                        var panelCommentAuth = auths.filter(auth=> auth._id == panelComment.userId)
+                        panelCommentAuth.forEach((a)=>{
+                            var commentData = {
+                                "name" : a.name,
+                                "surname" : a.surname,
+                                "textComment" : panelComment.textComment
+                            }
+                            comment.push(commentData)
+                        })
+                    })
+                    var panels = []
+                    var data = {
+                        //"_id" : panel._id,
+                        "name" : panelAuth.name,
+                        "surname" : panelAuth.surname,
+                        "text" : panel.text,
+                        "date" : panel.date,
+                        "comment" : comment,
+                        
+                        
+                    }
+                    
+                    panels.push(data)
+                    res.json({
+                        "panels":panels,
+                        "success":1
+                    })
+                })
+              
+            })
+        })
+    
+    
+        
+       /* panels.forEach((panel)=>{
+            panel.comment.forEach((element)=>{
+                Auth.find()
+                .then(auths=>{
+                    
+                 var commentAuth = auths.filter(auth=> auth._id == element.userId)
+               commentAuth.forEach((a)=>{
+                var data = {
+                    "_id":panel._id,
+                    "name":a.name,
+                    "surname":a.surname,
+                    "text":panel.text,
+                    "date":panel.date
+                }
+
+               })
+               
+               
+                
+                })
+            })
+            
+        })
+        
+      // res.json(comment)*/
 
     })
 })
@@ -19,7 +88,8 @@ router.post("/createPanel",(req,res)=>{
         userId:req.body.userId,
         text:req.body.text,
         date: req.body.date,
-        comment: req.body.comment
+        comment: req.body.comment,
+        areaId:req.body.areaId
     })
     panel.save()
     res.json("1")
